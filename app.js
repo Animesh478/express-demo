ccbf125a6aab7b417804f84c089fc4913f1367c6
Animesh478 <animeshmohanty.am@gmail.com>
2025-08-20 16:03:17 +0530

    handled dynamic routes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/welcome/:username", (req, res) => {
+  const username = req.params.username;
+  const role = req.query.role;
+
+  res.status(200).send(`Welcome ${username}, your role is ${role}`);
+});
+
+/*
 app.get("/products", (req, res) => {
   res.send("Here is the list of all products.");
 });
@@ -16,7 +24,7 @@ app.post("/categories", (req, res) => {
 app.use("/", (req, res) => {
   res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
-
+*/
 app.listen(4000, () => {
   console.log("Server is running on http://localhost:4000");
 });
