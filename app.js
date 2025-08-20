4d834eacc61741de61f0eca182d7d8a242fbba3e
Animesh478 <animeshmohanty.am@gmail.com>
2025-08-20 15:07:19 +0530

    created middleware

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use("/welcome", (req, res, next) => {
+  req.user = "Guest";
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
+
+app.get("/welcome", (req, res) => {
+  res.status(200).send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
 app.listen(3000, () => {
   console.log(
     "Server is up and running on port 3000! Ready to handle requests."
